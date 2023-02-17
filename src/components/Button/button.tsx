a29887b3d5b932ac306f165a1e4ff3978a611504
qiyu <157453032@qq.com>
2023-02-17 18:59:24 +0800

    add button组件部分样式

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,17 +1,8 @@
 import React from 'react'
 import classNames from 'classnames'
 
-export enum ButtonSize {
-  large = 'lg',
-  Small = 'sm',
-}
-
-export enum ButtonType {
-  Primary = 'primary',
-  Default = 'default',
-  Danger = 'danger',
-  Link = 'link',
-}
+type ButtonSize = 'lg' | 'sm'
+type ButtonType = 'primary' | 'default' | 'danger' | 'link'
 
 interface BaseButtonProps {
   className?: string
@@ -32,9 +23,9 @@ const Button: React.FC<ButtonProps> = (props) => {
   const classes = classNames('btn', className, {
     [`btn-${btnType}`]: btnType,
     [`btn-${size}`]: size,
-    disabled: btnType === ButtonType.Link && disabled,
+    disabled: btnType === 'link' && disabled,
   })
-  if (btnType === ButtonType.Link && href) {
+  if (btnType === 'link' && href) {
     return (
       <a className={classes} href={href} {...restProps}>
         {children}
@@ -51,7 +42,7 @@ const Button: React.FC<ButtonProps> = (props) => {
 
 Button.defaultProps = {
   disabled: false,
-  btnType: ButtonType.Default,
+  btnType: 'default',
 }
 
 export default Button
